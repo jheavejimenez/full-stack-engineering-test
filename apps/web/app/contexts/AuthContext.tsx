5f67e090a82f775107ba41d12db18473b8dc71f7
Jheave <jimenezjheave123@gmail.com>
2025-02-12 01:20:22 +0800

    add context manager

---
+++ apps/web/app/contexts/AuthContext.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import type React from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import type { DecodedToken, User } from '../lib/types';
+import { login as apiLogin, signup as apiSignup } from '../lib/api';
+import { jwtDecode } from 'jwt-decode';
+
+interface AuthContextType {
+  user: User | null;
+  role: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  signup: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState<User | null>(null);
+  const [role, setRole] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    const access_token = localStorage.getItem('access_token');
+    if (storedUser && access_token) {
+      setUser(JSON.parse(storedUser));
+      const decodedToken = jwtDecode<DecodedToken>(access_token);
+      setRole(decodedToken.role);
+    }
+  }, []);
+
+  const login = async (email: string, password: string) => {
+    const response = await apiLogin({ email, password });
+    const decodedToken = jwtDecode<DecodedToken>(response.access_token);
+    const user: User = {
+      id: decodedToken.sub,
+      name: decodedToken.name,
+      email: decodedToken.email,
+    };
+    setUser(user);
+    setRole(decodedToken.role);
+    localStorage.setItem('user', JSON.stringify(user));
+  };
+
+  const signup = async (name: string, email: string, password: string) => {
+    const response = await apiSignup({ name, email, password });
+    const decodedToken = jwtDecode<DecodedToken>(response.access_token);
+    const user: User = {
+      id: decodedToken.sub,
+      name: name,
+      email: decodedToken.email,
+    };
+    setUser(user);
+    setRole(decodedToken.role);
+    localStorage.setItem('user', JSON.stringify(user));
+    localStorage.setItem('access_token', response.access_token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setRole(null);
+    localStorage.removeItem('user');
+    localStorage.removeItem('access_token');
+  };
+
+  return <AuthContext.Provider value={{ user, role, login, signup, logout }}>{children}</AuthContext.Provider>;
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}
